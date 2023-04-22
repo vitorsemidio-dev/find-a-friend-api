e4f755d0d1d2218f496e4c1d66ce232dc9d2a281
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-04-22 12:28:21 -0300

    feat: generate random email when create org body

--- src/controllers/test/factories/make-create-org-body-schema.ts
+++ src/controllers/test/factories/make-create-org-body-schema.ts
@@ -1,12 +1,17 @@
 import { CreateOrgBodySchema } from '@/controllers/create-org.controller'
 
+const makeRandomEmail = () => {
+  const random = Math.random().toString(36).substring(7)
+  return `${random}@${random}.com`
+}
+
 export function makeCreateOrgBodySchema(
   override: Partial<CreateOrgBodySchema> = {},
 ): CreateOrgBodySchema {
   return {
     address: 'Rua do Pet, 123 - São Paulo - SP',
     cep: '00000-000',
-    email: 'find_a_friend@email.com',
+    email: makeRandomEmail(),
     name: 'Find A Friend Org',
     password: '123456',
     whatsappNumber: '5511999999999',
